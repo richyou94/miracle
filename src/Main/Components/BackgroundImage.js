65578d1bd385c42fc52b3e8032cebea4e5becacf
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2023-04-06 23:52:54 -0300

    Updates

--- src/Main/Components/BackgroundImage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./BackgroundImage.css";
-
-const BackgroundImage = (props) => {
-  return <div ref={props.imageRef} className="background-image">{props.children}</div>;
-};
-
-export default BackgroundImage;
